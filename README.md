6505d473d52c62ff855beda8d1030b59f85dbfc7
kalynsifuentez <kalynsifuentez@gmail.com>
2024-01-10 23:15:33 -0600

    added notes to README and updated the loaction of the css and JS files to labeled folders

--- README.md
+++ README.md
@@ -2,17 +2,18 @@
 # Challenge 6 - Weather Dashboard
 
 ## Description
-
+Weather Dashboard where users can check the current weather and 5-day forecast of the city that is searched. The city searched will save to local storage and be displayed as buttons to search this city again if the user chooses to do so. The weather dashboard displays the date, temperature, humidity, and wind speed.
 
 ## Code Source 
 
-HTTPS GitHub Repository: 
-Live URL: 
+HTTPS GitHub Repository: https://github.com/kalynsifuentez/weather-dashboard.git
+
+Live URL: https://kalynsifuentez.github.io/weather-dashboard/
 
 ## Screenshot !
-![Alt text](<assets/Screenshot of Application.png>)
+![Alt text](<assets/weather-dashboard.png>)
 ## Acknowledgements
-- Central Tutor Support:
+- Central Tutor Support: Jose Lopez
 
  - [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
  - [Awesome README](https://github.com/matiassingers/awesome-readme)
@@ -24,6 +25,4 @@ https://www.youtube.com/watch?v=YzYFuPp27Zw
 
 Great Stack 
 https://www.youtube.com/watch?v=MIYQR-Ybrn4&list=PLjwm_8O3suyOgDS_Z8AWbbq3zpCmR-WE9
-
-## Screenshots
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
+// Declared variables for each button, container, element and the API key
 const apiKey = "34992775f2f46c9b567e8fc38dba4aac";
 const searchBox = document.querySelector(".form");
 const searchBtn = document.querySelector("#search-form");
+const clearBtn = document.querySelector(".clear");
 const currentWeatherContainer = document.querySelector("#currentWeather");
 const forecastContainer = document.querySelector("#forecast");
 const searchedCitiesEl = document.querySelector("#searched-cities");
 let searchedCities = JSON.parse(localStorage.getItem("searchedCities")) || [];
 
+// checkWeather hosts the apiURL for same day forecast and fetches the apiKey
 function checkWeather(city) {
   const apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
@@ -15,34 +18,37 @@ function checkWeather(city) {
       return res.json();
     })
     .then(function (data) {
-      // console.log(data);
-
+      // Variables are declared for icon and date
       const iconcode = data.weather[0].icon;
-
       const iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-
       const dt = new Date(data.dt * 1000).toLocaleDateString();
 
+      // Elements are created and appended. These elements display the date, temperature, humidity, and wind
       const title = document.createElement("h3");
       const icon = document.createElement("img");
       const temp = document.createElement("p");
       const wind = document.createElement("p");
       const humid = document.createElement("p");
 
+      // Text is applied to each element
       title.textContent = data.name + " " + dt;
       temp.textContent = "Temperature: " + Math.round(data.main.temp) + "°F";
       wind.textContent = "Wind Speed: " + data.wind.speed + "MPH";
       humid.textContent = "Humidity: " + data.main.humidity + "%";
       icon.setAttribute("src", iconurl);
 
-      title.append(icon);
-      // console.log(temp);
+      // storeInLS function is called to store data in the local storage
       storeInLS(data.name);
 
+      // icon element is appended to the title element
+      title.append(icon);
+
+      // All elements are appended the currentWeatherContainer variable
       currentWeatherContainer.append(title, temp, wind, humid);
     });
 }
 
+// runFiveDay hosts the apiURL for 5-day forecast and fetches the apiKey
 function runFiveDay(city) {
   const apiUrl =
     "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=";
@@ -53,8 +59,10 @@ function runFiveDay(city) {
       return res.json();
     })
     .then(function (data) {
+      // For loop to extract every eighth element.
       for (let i = 0; i < data.list.length; i += 8) {
         console.log(data.list[i]);
+        // Array of variables declared to state day of the week instead of numerical date
         const dt = new Date(data.list[i].dt * 1000);
         const daysOfTheWeek = [
           "Sun",
@@ -68,72 +76,81 @@ function runFiveDay(city) {
 
         const dayOfTheWeek = daysOfTheWeek[dt.getDay()];
 
+        // To set box around the each element in 5-day forecast
         const forecastCard = document.createElement("div");
         forecastCard.setAttribute("class", "mx-2 p-2 card");
 
+        // Variables are declared for icon, date, and title
         const iconcode = data.list[i].weather[0].icon;
         const iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
+        // Created title,icon,temperature,wind, and humidity element
         const title = document.createElement("h5");
-        title.textContent = dayOfTheWeek + " " + dt.toLocaleDateString();
-
         const icon = document.createElement("img");
-        icon.setAttribute("src", iconurl);
-        title.append(icon);
         const temp = document.createElement("p");
         const wind = document.createElement("p");
         const humid = document.createElement("p");
+
+        // Text is applied to each element
+        title.textContent = dayOfTheWeek + " " + dt.toLocaleDateString();
         temp.textContent =
           "Temperature: " + Math.round(data.list[i].main.temp) + "°F";
         wind.textContent = "Wind Speed: " + data.list[i].wind.speed + "MPH";
         humid.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+        icon.setAttribute("src", iconurl);
 
+        // icon element is appended to the title element
+        title.append(icon);
+
+        // All elements are appended the currentWeatherContainer variable
         forecastCard.append(title, temp, wind, humid);
         forecastContainer.append(forecastCard);
       }
     });
 }
 
+// Event listener for search button
 searchBtn.addEventListener("submit", function (event) {
   event.preventDefault();
 
+  // Functions called with event listeners
   checkWeather(searchBox.value);
   runFiveDay(searchBox.value);
 });
 
+// Function to set recent searches to local storage
 function storeInLS(city) {
   if (searchedCities.includes(city)) {
     return;
   }
 
   searchedCities.push(city);
+  // Called loadButtons function to extract cities in local storage
   loadButtons();
   localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
 }
 
+// Function to have recent searches appear in button form to click and search city again
 function loadButtons() {
   searchedCitiesEl.innerHTML = "";
   for (let i = 0; i < searchedCities.length; i++) {
+    // declared variable to create button element
     const searchedCitiesList = document.createElement("button");
+    // Added class to button
     searchedCitiesList.classList.add("w-50");
-
+    // City text to display in button
     searchedCitiesList.textContent = searchedCities[i];
-
+    // Added event listener to buttons that are created from local storage
     searchedCitiesList.addEventListener("click", function () {
       checkWeather(this.textContent);
       runFiveDay(this.textContent);
     });
-
+// Appended searchedCitiesList to searched CitiesEl to display on HTML page
     searchedCitiesEl.appendChild(searchedCitiesList);
   }
 }
-// clearBtn.addEventListener("click", () => {
-//   localStorage.clear();
-//   resultEl.innerText = '';
-//   resultNameEl.innerText = '';
-// });
 
+// Run checkWeather, runFiveDay, and loadButtons to display when user loads page
 checkWeather("new york");
 runFiveDay("new york");
-
 loadButtons();
